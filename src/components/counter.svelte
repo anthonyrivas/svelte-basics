9bb104d2fe7d6b238c11ed84e803811ce6b08474
TJ Rivas <tj.rivas@opensesame.com>
2024-01-23 07:34:11 -0700

    Initial setup

---
+++ src/components/counter.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Button from "components/button.svelte";
+
+  let count = 0;
+</script>
+
+<h2>Count: </h2>
+<div id="count"> </div>
+<Button> + 1 </Button>
+
+<style>
+  h2 {
+    color: red;
+  }
+  #count {
+    color: blue;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import Counter from "components/counter.svelte";
+
+</script>
+<h1>OpenSesame Svelte Workshop 2024</h1>
+<Counter />
